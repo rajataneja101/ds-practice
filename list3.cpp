aa1a59454e3e654e2aca8f1753ddc1b6d1b08773
Rajat Taneja <tanejarajat101@gmail.com>
2017-06-09 11:17:18 +0530

    Delete a node

---
+++ list3.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<malloc.h>
+using namespace std;
+struct node
+{
+int data;
+struct node *next;
+};
+struct node *head=NULL;
+struct node *prev,*curr;
+void insert(int n)
+{
+struct node *newnode;
+newnode=new node;
+newnode->data=n;
+newnode->next=NULL;
+if(head==NULL)
+{
+head=newnode;
+return;
+}
+if(newnode->data<=head->data)
+{
+newnode->next=head;
+head=newnode;
+return;
+}
+prev=curr;
+for(curr=head;curr!=NULL;curr=curr->next)
+{
+if(newnode->data<curr->data)break;
+else
+prev=curr;
+}
+newnode->next=prev->next;
+prev->next=newnode;
+}
+void traverse()
+{
+for(curr=head;curr!=NULL;curr=curr->next)
+cout<<curr->data;
+}
+void deletenode()
+{
+if(head==NULL)
+{
+cout<<"Node is empty!!";
+return;
+}
+int n;
+cout<<"Enter data to delete";
+cin>>n;
+if(head->data==n)
+{
+head=head->next;
+return;
+}
+prev=curr=NULL;
+for(curr=head;curr!=NULL;curr=curr->next)
+{
+if(curr->data==n)break;
+else
+prev=curr;
+}
+if(curr==NULL)
+{
+cout<<"Not found";
+return;
+}
+else
+{
+prev->next=curr->next;
+}
+}
+int main()
+{
+int inf, ch;
+while(1)
+{ cout<< " \n\n\n\n Linked List Operations\n\n";
+cout<< " 1- Add Node \n 2- Delete Node \n";
+cout<< " 3- Traverse List \n 4- exit\n";
+cout<< "\n\n Your Choice: "; cin>>ch;
+switch(ch)
+{ case 1: cout<< "\n Put info/value to Add: ";
+cin>>inf;
+insert(inf);
+break;
+case 2:deletenode(); 
+break;
+case 3: cout<< "\n Linked List Values:\n";
+traverse(); break;
+case 4: //exit(0);
+break;
+} // end of switch
+} // end of while loop
+return 0;
+} // end of main ( ) function
+
+
